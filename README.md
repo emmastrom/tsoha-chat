1e5c06bee006cbfd821301edf4aa4c1808881483
Emma Lundström <1emma.lundstrom@gmail.com>
2021-05-09 23:41:07 +0300

    readme and routes

--- README.md
+++ README.md
@@ -4,10 +4,10 @@ Sovelluksessa näkyy keskustelualueita, joista jokaisella on tietty aihe. Alueil
 Sovelluksen ominaisuuksia:
 
 -  Käyttäjä voi kirjautua sisään ja ulos sekä luoda uuden tunnuksen.
--  Käyttäjä näkee sovelluksen etusivulla listan alueista sekä jokaisen alueen ketjujen ja viestien määrän ja viimeksi lähetetyn viestin ajankohdan.
+-  Käyttäjä näkee sovelluksen etusivulla listan alueista sekä jokaisen alueen ketjujen ja viestien määrän.
 -  Käyttäjä voi luoda alueelle uuden ketjun antamalla ketjun otsikon ja aloitusviestin sisällön.
 -  Käyttäjä voi kirjoittaa uuden viestin olemassa olevaan ketjuun.
--  Käyttäjä voi muokata luomansa ketjun otsikkoa sekä lähettämänsä viestin sisältöä. Käyttäjä voi myös poistaa ketjun tai viestin.
+-  Käyttäjä voi muokata luomansa ketjun otsikkoa sekä lähettämänsä aloitusviestin sisältöä.
 -  Ylläpitäjä voi lisätä ja poistaa keskustelualueita.
 
 Toimii [Herokussa](https://pacific-retreat-24717.herokuapp.com/).
--- routes.py
+++ routes.py
@@ -17,16 +17,27 @@ def index():
 def add_area():
     if request.method == "GET":
         return render_template("add.html")
+
     if request.method == "POST":
+
         name = request.form["name"]
+        if len(name) < 1 or len(name) > 24:
+            return render_template("error.html", message="Nimessä pitää olla 1-24 merkkiä")
         areas.add_area(name, users.user_id())
         return redirect("/")
 
 @app.route("/create", methods=["POST"])
 def create_chain():
     if request.method == "POST":
+
         subject = request.form["subject"]
+        if len(subject) < 1 or len(subject) > 50:
+            return render_template("error.html", message="Aiheessa pitää olla 1-50 merkkiä")
+
         opening_message = request.form["opening_message"]
+        if len(opening_message) > 200:
+            return render_template("error.html", message="Aloitusviestissä saa olla enintään 200 merkkiä")
+
         area_id = request.form["area_id"]
         if users.user_id() == 0:
             render_template("error.html", message="Ketjun luominen epäonnistui. Tarkista, että olet kirjautunut sisään")
@@ -41,6 +52,7 @@ def remove_area():
         return render_template("remove.html", list=list)
 
     if request.method == "POST":
+
         if "area" in request.form:
             area_id = request.form["area"]
             areas.remove_area(area_id, users.user_id())
@@ -53,6 +65,7 @@ def edit_subject():
         return render_template("edit_subject.html", list=list)
 
     if request.method == "POST":
+
         if "chain" in request.form:
             chain_id = request.form["chain"]
             subject = request.form["subject"]
@@ -66,6 +79,7 @@ def edit_opening_message():
         return render_template("edit_opening_message.html", list=list)
 
     if request.method == "POST":
+
         if "chain" in request.form:
             chain_id = request.form["chain"]
             opening_message = request.form["opening_message"]
@@ -90,6 +104,8 @@ def area(id):
 @app.route("/send", methods=["POST"])
 def send():
     content = request.form["content"]
+    if len(content) < 1 or len(content) > 200:
+        render_template("error.html", message="Viestin pitää olla 1-200 merkkiä pitkä")
     chain_id = request.form["chain_id"]
     messages.send(content, chain_id)
     return redirect("/chain/"+chain_id)
@@ -98,9 +114,11 @@ def send():
 def login():
     if request.method == "GET":
         return render_template("login.html")
+
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
+
         if users.login(username, password):
             return redirect("/")
         else:
@@ -118,6 +136,9 @@ def register():
 
     if request.method == "POST":
         username = request.form["username"]
+
+        if len(username) < 1 or len(username) > 24:
+            return render_template("error.html", message="Tunnuksessa pitää olla 1-24 merkkiä")
 
         password = request.form["password"]
 
